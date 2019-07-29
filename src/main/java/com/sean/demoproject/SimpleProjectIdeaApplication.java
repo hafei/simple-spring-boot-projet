58b09491e65ed9cf91887a3315d769f59f393eeb
hafei <zhangmengcoding@gmail.com>
2019-07-29 23:46:56 +0800

    add logging

--- src/main/java/com/sean/demoproject/SimpleProjectIdeaApplication.java
+++ src/main/java/com/sean/demoproject/SimpleProjectIdeaApplication.java
@@ -1,13 +1,16 @@
 package com.sean.demoproject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimpleProjectIdeaApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(SimpleProjectIdeaApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(SimpleProjectIdeaApplication.class, args);
+        logger.info("project started");
     }
-
 }
