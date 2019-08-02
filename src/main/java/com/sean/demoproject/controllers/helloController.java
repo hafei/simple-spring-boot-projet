2b7acfd4a6f83d2762460546f3bb15317115f8b3
hafei <zhangmengcoding@gmail.com>
2019-08-02 23:44:07 +0800

    add Swagger2

--- src/main/java/com/sean/demoproject/controllers/helloController.java
+++ src/main/java/com/sean/demoproject/controllers/helloController.java
@@ -1,7 +1,12 @@
 package com.sean.demoproject.controllers;
 
+
 import com.sean.demoproject.model.Pledge;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,17 +14,22 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping(value = "/hello")
 public class helloController {
 
+    private Logger logger = LoggerFactory.getLogger(helloController.class);
     private AtomicLong nextId = new AtomicLong();
 
+    @ApiOperation(value = "Hello")
     @GetMapping("/hello")
     public String getHello() {
+        logger.info("Hello Spring Boot");
         return "Hello Spring Boot!";
     }
 
     @GetMapping("/id")
     public long id() {
+        logger.info("/id");
         return nextId.incrementAndGet();
     }
 
